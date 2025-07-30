c4acde1567e79fb26cb3d26947382a9ced3116b8
pihu8094 <pihukumari28255@gmail.com>
2025-07-31 01:38:12 +0530

    Update 000000paglu.js

--- models/commands/000000paglu.js
+++ models/commands/000000paglu.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(350, 350), 90, 175).composite(circleTwo.resize(350, 350), 825, 175);
+    batgiam_img.composite(circleOne.resize(350, 350), 90, 170).composite(circleTwo.resize(350, 350), 825, 168);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
