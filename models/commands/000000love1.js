47910a5234e56b015a7c9fccbb6c83544e0e1b42
pihu8094 <pihukumari28255@gmail.com>
2025-07-31 19:09:11 +0530

    Update 000000love1.js

--- models/commands/000000love1.js
+++ models/commands/000000love1.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(335, 335), 90, 175).composite(circleTwo.resize(335, 335), 825, 172);
+    batgiam_img.composite(circleOne.resize(360, 360), 90, 175).composite(circleTwo.resize(360, 360), 825, 172);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
