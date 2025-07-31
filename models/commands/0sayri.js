bba0960d6a93489cee7a0cc0d5b07b8dc9e1a183
pihu8094 <pihukumari28255@gmail.com>
2025-07-31 21:39:35 +0530

    Update 0sayri.js

--- models/commands/0sayri.js
+++ models/commands/0sayri.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(380, 380), 160, 140).composite(circleTwo.resize(10, 10), 5, 5);
+    batgiam_img.composite(circleOne.resize(380, 380), 158, 138).composite(circleTwo.resize(10, 10), 5, 5);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
