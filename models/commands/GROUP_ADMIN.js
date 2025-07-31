34430b6ba2b6d76179596fc0c95766639b00f254
pihu8094 <pihukumari28255@gmail.com>
2025-07-31 22:12:16 +0530

    Update GROUP_ADMIN.js

--- models/commands/GROUP_ADMIN.js
+++ models/commands/GROUP_ADMIN.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(10, 10), 5, 5).composite(circleTwo.resize(300, 300), 450, 172);
+    batgiam_img.composite(circleOne.resize(10, 10), 5, 5).composite(circleTwo.resize(300, 300), 465, 195);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
