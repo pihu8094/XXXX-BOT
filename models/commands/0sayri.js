fc76520d00a6b1cd82b9f79ec12824b42652f8e6
pihu8094 <pihukumari28255@gmail.com>
2025-07-31 21:19:07 +0530

    Update and rename 1.js to 0sayri.js

--- models/commands/0sayri.js
+++ models/commands/0sayri.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(350, 350), 90, 170).composite(circleTwo.resize(0, 0), 0, 0);
+    batgiam_img.composite(circleOne.resize(350, 350), 90, 170).composite(circleTwo.resize(360, 369), 975, 200);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
