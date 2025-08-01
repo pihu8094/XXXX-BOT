298269fcf2b0b0a4dab9d945c019d055fe14e8b6
pihu8094 <pihukumari28255@gmail.com>
2025-08-01 11:34:29 +0530

    Update DAY1.js

--- models/commands/DAY1.js
+++ models/commands/DAY1.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(10, 10), 5, 5).composite(circleTwo.resize(500, 500), 700, 168);
+    batgiam_img.composite(circleOne.resize(10, 10), 5, 5).composite(circleTwo.resize(500, 500), 500, 168);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
