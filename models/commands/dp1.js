1938d3c9d87447b3cbcbe5f5351bc5de5213b75d
pihu8094 <pihukumari28255@gmail.com>
2025-08-05 20:09:34 +0530

    Update dp1.js

--- models/commands/dp1.js
+++ models/commands/dp1.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(250, 250), 110, 180).composite(circleTwo.resize(250, 250), 700, 180);
+    batgiam_img.composite(circleOne.resize(250, 250), 110, 195).composite(circleTwo.resize(250, 250), 700,200);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
