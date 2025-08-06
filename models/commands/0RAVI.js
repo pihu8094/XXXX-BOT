7e1beb6407f9b549ac3f16612546015b8c3fe1c4
pihu8094 <pihukumari28255@gmail.com>
2025-08-06 14:08:41 +0530

    Update 0RAVI.js

--- models/commands/0RAVI.js
+++ models/commands/0RAVI.js
@@ -92,8 +92,8 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   let canvas = createCanvas(baseImage.width, baseImage.height);
   let ctx = canvas.getContext("2d");
   ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-  ctx.drawImage(baseAvt1, 45, 60, 280, 320);
-  ctx.drawImage(baseAvt2, 390, 300, 280, 320);
+  ctx.drawImage(baseAvt1, 90, 80, 280, 320);
+  ctx.drawImage(baseAvt2, 420, 320, 280, 320);
   const imageBuffer = canvas.toBuffer();
   fs.writeFileSync(pathImg, imageBuffer);
   fs.removeSync(pathAvt1);
